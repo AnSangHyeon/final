6babae579d84903dbf20b9ace8851733dc8df340
AnSangHyeon <ash040906@naver.com>
2025-06-12 13:16:57 +0900

    세번쨰커밋(주석)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+// Final컴포넌트 임포트
 import Final from './Final';
 
 function App() {
   return (
     <div className="App">
+      {/* Final 컴포넌트 가져옴 */}
       <Final />
     </div>
   );
--- src/Final.js
+++ src/Final.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
 let Final = () => {
-  const [color, setColor] = useState("purple");
+  // color를 받아올 훅
+  const [color, setColor] = useState("Purple");
   return(
     <div>
       <div 
+        // backgroundColor를 state의 변수로 설정
         style={{
           background: color
         }}
       >
+        {/* 사용자가 입력한 색 출력 */}
         <span>Background Color : {color}</span>
+
+        {/* input이벤트로 텍스트가 바뀔떄마다 input의value를 가져와서 setColor로 색상 동적으로 바꿔줌 */}
         <input type="text" value={color} onInput={() => setColor(document.querySelector("input[type=text]").value)}/>
       </div>
     </div>

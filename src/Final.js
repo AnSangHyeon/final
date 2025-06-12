8df1915e2d54627d1f2e3d3933db2ecd5b2fe627
AnSangHyeon <ash040906@naver.com>
2025-06-12 13:13:58 +0900

    두번쨰 커밋

--- src/Final.js
+++ src/Final.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 let Final = () => {
+  const [color, setColor] = useState("purple");
   return(
-    <>
-      123
-    </>
+    <div>
+      <div 
+        style={{
+          background: color
+        }}
+      >
+        <span>Background Color : {color}</span>
+        <input type="text" value={color} onInput={() => setColor(document.querySelector("input[type=text]").value)}/>
+      </div>
+    </div>
   );
 }
 export default Final;
